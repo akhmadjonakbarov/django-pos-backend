32dc092eae3ea348d9d20dbaf66c41c6036627fa
Akhmadjon Akbarov <97084545+akhmadjonakbarov@users.noreply.github.com>
2024-10-08 22:23:23 +0500

    ref: return product part was done

--- apps/product_app/views/doc_item_views.py
+++ apps/product_app/views/doc_item_views.py
@@ -2,12 +2,15 @@ from django.db import transaction
 from rest_framework.generics import GenericAPIView
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, AllowAny, IsAuthenticated
+from rest_framework.request import Request
+
 from ..models import DocumentItemModel, DocumentModel, DocumentItemBalanceModel
 
 from apps.utils.response_type import res_error, response_list, res_message
 from ..serializers.document_item_balance_serializers import DocumentItemBalanceModelSerializer
 from ...base_app.views import BaseListView
 from ..serializers import doc_item_serializers
+from ...spiska_app.models import SpiskaModel
 from ...utils.response_messages import ResponseMessages
 
 
@@ -58,43 +61,46 @@ class BuyListView(BaseView):
 class DeleteView(BaseView):
     permission_classes = (IsAuthenticated,)
 
-    def delete(self, request, pk):
+    def delete(self, request: Request, pk):
         try:
             with transaction.atomic():
-                doc_item: DocumentItemModel = self.queryset.active_objects().get(pk=pk)
-                print(doc_item)
+                doc_item: DocumentItemModel = self.queryset.active_objects().get(
+                    pk=pk,
+                    document__doc_type=DocumentModel.SELL,
+                )
                 try:
-                    item_balance = DocumentItemBalanceModel.active_objects().get(
-                        doc_item=doc_item
+                    item_balance: DocumentItemBalanceModel = DocumentItemBalanceModel.active_objects().get(
+                        item=doc_item.item
                     )
 
                     item_balance.qty += doc_item.qty
                     item_balance.save()
                 except DocumentItemBalanceModel.DoesNotExist:
                     DocumentItemBalanceModel.objects.create(
-                        item=doc_item.item,
                         doc_item=doc_item,
-                        currency=doc_item.currency,
-                        user=doc_item.user,
-                        selling_price=doc_item.selling_price,
-                        selling_percentage=doc_item.selling_percentage,
-                        document_id=doc_item.document_id,
-                        income_price=doc_item.income_price,
-                        income_price_usd=doc_item.income_price_usd,
+                        item=doc_item.item,
                         qty=doc_item.qty,
-                        can_be_cheaper=doc_item.can_be_cheaper,
-                        discount_price=doc_item.discount_price,
+                        income_price=doc_item.income_price,
+                        selling_price=doc_item.selling_price,
+                        user=request.user,
+                        currency=doc_item.currency,
+                        income_price_usd=doc_item.income_price_usd,
+                        selling_percentage=doc_item.selling_percentage,
+                        document=doc_item.document
                     )
+                    spiska: SpiskaModel = SpiskaModel.active_objects().filter(item=doc_item.item).first()
+                    if spiska:
+                        spiska.hard_delete(spiska.pk)
 
-                document: DocumentModel = DocumentModel.active_objects().get(
-                    id=doc_item.document_id
+                document_sell: DocumentModel = DocumentModel.active_objects().get(
+                    id=doc_item.document_id, doc_type=DocumentModel.SELL
                 )
-                doc_items = document.items.all()
+                doc_items = document_sell.items.all().filter(is_deleted=False)
                 if doc_items.count() > 1:
                     doc_item.delete()
                 else:
                     doc_item.delete()
-                    document.delete()
+                    document_sell.delete()
                 return res_message(message=ResponseMessages.SUCCESS, status=status.HTTP_200_OK)
         except Exception as e:
             print(e)
